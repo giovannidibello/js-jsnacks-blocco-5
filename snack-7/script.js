d0e07346877e28591282665bd97558ae67d66d89
Giovanni <dibellogio@gmail.com>
2025-01-21 14:52:12 +0100

    add Snack 7 solution

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,10 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+const student2 = students.find((student) => {
+  return student.id === 2;
+});
+
+console.log(student2);
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
