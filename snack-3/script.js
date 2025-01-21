c634e2fe3491ac0c5099a198541a569592a62a6f
Giovanni <dibellogio@gmail.com>
2025-01-21 14:38:37 +0100

    add Snack 3 solution

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const increaseNumbers = numbers.map((number) => {
+    return ++number;
+})
+
+console.log(increaseNumbers);
+
+
 // Risultato: [3, 9, 5, 8, 3, 88]
